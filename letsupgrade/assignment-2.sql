541e9eb74852a38036d5ccf617c82a2f487f325d
Tharun <72075661+Tharun1616@users.noreply.github.com>
2023-06-07 20:15:57 +0530

    Create assignment-2.sql

---
+++ letsupgrade/assignment-2.sql
@@ -0,0 +1,11 @@
+# Create a database called "sales" with a table called "orders" that contains the following columns: 
+# order_id (int), customer_id (int), order_date (date), order_total (decimal).
+
+CREATE DATABASE sales;
+
+CREATE TABLE orders (
+  order_id INT,
+  customer_id INT,
+  order_date DATE,
+  order_total DECIMAL
+);
