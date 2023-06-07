d703d3c1002415f61b70fb23c2270c569fc4136c
Tharun <72075661+Tharun1616@users.noreply.github.com>
2023-06-07 11:06:45 +0530

    Create assignment1.sql

---
+++ letsupgrade/assignment1.sql
@@ -0,0 +1,9 @@
+## "Using SQL, create a database for a simple todo list program that has the following columns: 
+## task_id (primary key), task_name, description, and is_completed."
+
+CREATE TABLE todo_list (
+  task_id INT PRIMARY KEY,
+  task_name VARCHAR(255),
+  description TEXT,
+  is_completed BOOLEAN
+);
